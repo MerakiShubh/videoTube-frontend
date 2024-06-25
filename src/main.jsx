590439338879e4747e9a08432c22d90789457d13
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-26 00:05:05 +0530

    configure redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { Provider } from "react-redux";
+import { store } from "./utils/store.js";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </QueryClientProvider>
 );
