8e083ed3f818847f6f7f5049d0d965a362c341ad
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-27 23:55:11 +0530

    fetch all vides in home comonent

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import Card from "../components/Card";
+import { useQuery } from "@tanstack/react-query";
+import { fetchVideos } from "../HTTP/api";
+import { Loader } from "lucide-react";
 
 const Container = styled.div`
   display: flex;
@@ -7,29 +10,43 @@ const Container = styled.div`
   flex-wrap: wrap;
 `;
 
+const SpinLoader = styled(Loader)`
+  animation: spin 1s linear infinite;
+  @keyframes spin {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
 const Home = () => {
+  const {
+    isLoading,
+    isError,
+    data: videos,
+    error,
+  } = useQuery({
+    queryKey: ["videos"],
+    queryFn: fetchVideos,
+  });
+
+  if (isLoading) {
+    return (
+      <div>
+        <SpinLoader />
+      </div>
+    );
+  }
+
+  if (isError) {
+    return <div>Some Error occurred: {error.message}</div>;
+  }
+
   return (
     <Container>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {videos.map((video) => (
+        <Card key={video._id} video={video} />
+      ))}
     </Container>
   );
 };
