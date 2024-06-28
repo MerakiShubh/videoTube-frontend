05834e5199c8907beaf26ad4b7971b35cbe497cd
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-28 19:10:05 +0530

    add redux persistance

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
-import { store } from "./utils/store.js";
+import { store, persistor } from "./utils/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </PersistGate>
   </Provider>
 );
--- src/utils/store.js
+++ src/utils/store.js
@@ -1,7 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+// Import necessary modules
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+import videoReducer from "./videoSlice";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // or import localStorage
+
+// Define persist configuration
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+// Combine reducers
+const rootReducer = combineReducers({
+  user: userReducer,
+  video: videoReducer,
 });
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Configure store with persisted reducer and middleware
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/FLUSH",
+          "persist/PAUSE",
+          "persist/PURGE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
+});
+
+// Create persistor
+export const persistor = persistStore(store);
