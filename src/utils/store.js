b8286fb7c1890351af43417d1f165135a623c6e7
MerakiShubh <shubhamt.champ@gmail.com>
2024-06-30 19:20:53 +0530

    Managing state of comments

--- src/utils/store.js
+++ src/utils/store.js
@@ -4,6 +4,7 @@ import userReducer from "./userSlice";
 import videoReducer from "./videoSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import commentSlice from "./commentSlice";
 
 const persistConfig = {
   key: "root",
@@ -14,6 +15,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   video: videoReducer,
+  comment: commentSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
